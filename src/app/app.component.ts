fa53993546dfb56020a729ac4e91f50851e9d982
Chu Văn Giá <vangiachu@gmail.com>
2023-10-19 14:28:40 +0700

    add rxjs observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,15 @@ import { WikipediaService } from './wikipedia.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   pages: any[] = [];
   constructor(private wikepedia: WikipediaService) {}
 
   onTerm(term: string) {
-    this.wikepedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
-    })
+    this.wikepedia.search(term).subscribe((pages) => {
+      this.pages = pages;
+    });
   }
 }
